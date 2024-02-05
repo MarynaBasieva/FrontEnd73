5d7130973d23a063c190256235aeb13ec2906c74
FullRepo <marfa1981@ukr.net>
2024-02-05 16:29:13 +0000

    hw14

---
+++ HW_Student/Levytska_Alesia/HW14/js/customs.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function(){
+	
+	jQuery('.btn_banner').on('click', function(){
+		jQuery('.parent_modal').fadeIn('slow');
+	});
+
+	jQuery('.close').on('click', function(){
+		jQuery('.parent_modal').fadeOut('slow');
+	});
+});
